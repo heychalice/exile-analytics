9c65e07cd4c450ba7cbd03bba58a5023fd5fe1e5
zixm <i.ye@mail.utoronto.ca>
2026-03-02 22:48:39 -0500

    vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_BASE = "http://localhost:8080";
+const API_BASE = "https://exile-analytics.vercel.app/";
 const league = "Keepers";
 const CACHE_TTL_MS = 120 * 60 * 1000;
 
